3d049ee12205171cd6faed3184639b4da468bf05
Zen6ka <131902026+Zen6ka@users.noreply.github.com>
2024-01-10 18:00:38 +0200

    les

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+const jacob = {
+  id: '1',
+  name: "Jacob Gluke",
+  email: 'j.Merser@gmail.com',
+	age: 32,
+	avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+};
+
+
+
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>
+		<div> 
+<p><b>User name:</b> {jacob.name}</p>
+<p><b>E-mail:</b>{jacob.email}</p>
+<p><b>Age:</b>{jacob.age}</p>
+<img src = {jacob.avatar} alt = {jacob.name} width="80" height="80"/>
+</div>
+	</div>;
 };
